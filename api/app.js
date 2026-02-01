7436a5ecdd57dba1c802d4ff35b8e60b2f3b0b8f
Santosh raj <santoshrajgupta7366@gmail.com>
2026-02-01 23:50:19 +0530

    Added chat route

--- api/app.js
+++ api/app.js
@@ -5,6 +5,8 @@ import authRoute from "./routes/auth.route.js";
 import postRoute from "./routes/post.route.js";
 import testRoute from "./routes/test.route.js";
 import userRoute from "./routes/user.route.js";
+import chatRoute from "./routes/chat.route.js";
+import messageRoute from "./routes/message.route.js";
 
 
 const app = express();
@@ -17,6 +19,8 @@ app.use("/api/auth", authRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/test", testRoute);
 app.use("/api/users", userRoute);
+app.use("/api/chats", chatRoute);
+app.use("/api/messages", messageRoute);
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000");
